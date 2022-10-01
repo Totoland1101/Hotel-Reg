c8f682b7e5b64c41b5bafae8bde986056a5ff023
Night_J <>
2022-10-01 10:42:28 +0700

    Object To Create Code

---
+++ Create Database.py
@@ -0,0 +1,9 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+      host = "localhost",
+    user="root",
+    password="1234567890",
+   
+)
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE cs_project")
---
+++ Create Table.py
@@ -0,0 +1,21 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+      host = "localhost",
+    user="root",
+    password="1234567890",
+    database="cs_project"
+)
+mycursor = mydb.cursor()
+mycursor.execute("CREATE TABLE RID (int AUTO_INCREMENT, NAME varchar(255) , CONTACT varchar(255), EMAIL varchar(255), GENDER varchar(255), CITY varchar(255), STATE varchar(255),PRIMARY KEY (RID)")
+
+
+# CREATE TABLE REG (
+#     RID int NOT NULL AUTO_INCREMENT,
+#     NAME varchar(255) NOT NULL,
+#     CONTACT varchar(255),
+#     EMAIL varchar(255),
+#     GENDER varchar(255),
+#     CITY varchar(255),
+#     STATE varchar(255),
+#     PRIMARY KEY (RID)
+# );
